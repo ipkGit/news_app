84ebb74ca70526d5205d3cea50bf021a75e150b2
Kripak Igor <ipkforwork@gmail.com>
2023-01-23 21:35:45 +0500

    fix add @JsonIgnore above field news in Origin

--- src/main/java/com/example/news_app/entity/Origin.java
+++ src/main/java/com/example/news_app/entity/Origin.java
@@ -1,5 +1,6 @@
 package com.example.news_app.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,6 +27,7 @@ public class Origin {
     private String name;
 
     @OneToMany(mappedBy = "origin", fetch = FetchType.LAZY)
+    @JsonIgnore
     private List<News> news;
 
     @Override
